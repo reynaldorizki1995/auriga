e5ece95635fefb7d8d9e0def1f1b2a541fb47ede
reynaldorizki1995 <60774754+reynaldorizki1995@users.noreply.github.com>
2022-05-22 07:17:13 +0700

    First Commit

---
+++ application/views/_includes/public/footer.php
@@ -0,0 +1,19 @@
+</div>
+<footer class="footer footer-transparent d-print-none">
+</footer>
+</div>
+</div>
+<!-- Libs JS -->
+<script src="<?= base_url(); ?>assets/dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jquery/dist/jquery.slim.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/apexcharts/dist/apexcharts.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jqvmap/dist/jquery.vmap.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<!-- Tabler Core -->
+<script src="<?= base_url(); ?>assets/dist/js/tabler.min.js"></script>
+<script>
+    document.body.style.display = "block"
+</script>
+</body>
+
+</html>
